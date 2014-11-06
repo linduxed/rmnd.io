bf8f9c4bd111831d706c23cf6139a15f125effb0
Carl Olof Erlandsson <calle@thoughtbot.com>
2014-11-06 20:38:49 +0100

    Configure mailer sender using environment var.
    The default mailer sender is configured using the `MAILER_SENDER`
    environment variable.

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,6 +1,8 @@
 class Mailer < ActionMailer::Base
+  default from: ENV.fetch("MAILER_SENDER")
+
   def reminder(reminder)
     @reminder = reminder
-    mail to: reminder.email, subject: reminder.title
+    mail to: @reminder.email, subject: @reminder.title
   end
 end
--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,3 +1,3 @@
 Clearance.configure do |config|
-  config.mailer_sender = "noreply@remind-eu.herokuapp.com"
+  config.mailer_sender = ENV.fetch("MAILER_SENDER")
 end
