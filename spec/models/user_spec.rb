6aff467689c9063f3ab09f359e7ea5d1d5a29585
Carl Olof Erlandsson <calle@thoughtbot.com>
2014-11-09 13:01:47 +0100

    Tweak layout and styles.
    * Adjust colors.
    * Add copy.
    * Harmonize sign in and sign up views/
    * Clarify which fields are required when signing up.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,9 @@ describe User do
   describe "assocations" do
     it { should have_many(:reminders).dependent(:destroy) }
   end
+
+  describe "validations" do
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password) }
+  end
 end
